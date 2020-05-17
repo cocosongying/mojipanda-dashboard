887922be98304412d252116813c4bf1978193310
cocosongying <cocosongying@gmail.com>
2020-05-17 17:49:52 +0800

    feat:用户详情页可修改信息和密码

--- src/common/user.js
+++ src/common/user.js
@@ -11,6 +11,16 @@ const User = {
         let url = Consts.Api.BASE_URL + Consts.Api.USER_GETBY_ID;
         let res = await axios.post(url, params);
         return res.data;
+    },
+    async updateById(params) {
+        let url = Consts.Api.BASE_URL + Consts.Api.USER_UPDATEBY_ID;
+        let res = await axios.post(url, params);
+        return res.data;
+    },
+    async modifyPasswd(params) {
+        let url = Consts.Api.BASE_URL + Consts.Api.USER_MODIFY_PWD;
+        let res = await axios.post(url, params);
+        return res.data;
     }
 }
 
--- src/components/menu/Profile.vue
+++ src/components/menu/Profile.vue
@@ -13,7 +13,6 @@
             <div class="box-body box-profile">
               <img class="profile-user-img img-responsive img-circle" :src="userInfo.avatar" />
               <h3 class="profile-username text-center">{{ userInfo.nickname }}</h3>
-              <p class="text-muted text-center">Software Engineer</p>
               <a href="#" class="btn btn-block bg-red">
                 <b>好运</b>
               </a>
@@ -30,20 +29,9 @@
             <!-- /.box-header -->
             <div class="box-body">
               <strong>
-                <i class="fa fa-pencil margin-r-5"></i> 技能
+                <i class="fa fa-file-text-o margin-r-5"></i> 描述
               </strong>
-              <p>
-                <button class="label bg-navy">Coding</button>
-                <button class="label label-info">Javascript</button>
-                <button class="label label-warning">PHP</button>
-                <button class="label label-primary">Node.js</button>
-              </p>
-              <hr />
-
-              <strong>
-                <i class="fa fa-file-text-o margin-r-5"></i> 备注
-              </strong>
-              <p>备注信息</p>
+              <p>{{ userInfo.description }}</p>
             </div>
             <!-- /.box-body -->
           </div>
@@ -54,56 +42,86 @@
           <div class="nav-tabs-custom">
             <ul class="nav nav-tabs">
               <li class="active">
-                <a href="#settings" data-toggle="tab">设置</a>
+                <a href="#settings" data-toggle="tab">修改信息</a>
+              </li>
+              <li class>
+                <a href="#password" data-toggle="tab">修改密码</a>
               </li>
             </ul>
             <div class="tab-content">
               <div class="active tab-pane" id="settings">
                 <form class="form-horizontal">
                   <div class="form-group">
-                    <label for="inputName" class="col-sm-2 control-label">登录名</label>
-
+                    <label class="col-sm-2 control-label">登录名</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" :value="userInfo.username" disabled />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label">昵称</label>
                     <div class="col-sm-10">
                       <input
-                        type="email"
+                        type="text"
                         class="form-control"
-                        id="inputName"
-                        placeholder="Name"
-                        disabled
+                        v-model="nickname"
+                        :disabled="disabled"
                       />
                     </div>
                   </div>
                   <div class="form-group">
-                    <label for="inputEmail" class="col-sm-2 control-label">Email</label>
-
+                    <label class="col-sm-2 control-label">描述</label>
                     <div class="col-sm-10">
-                      <input type="email" class="form-control" id="inputEmail" placeholder="Email" />
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputName" class="col-sm-2 control-label">昵称</label>
-
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputName" placeholder="Name" />
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputSkills" class="col-sm-2 control-label">技能</label>
-
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputSkills" placeholder="Skills" />
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputExperience" class="col-sm-2 control-label">备注</label>
-
-                    <div class="col-sm-10">
-                      <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                      <textarea class="form-control" v-model="description" :disabled="disabled"></textarea>
                     </div>
                   </div>
                   <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
-                      <button type="submit" class="btn btn-danger">提交</button>
+                      <a class="btn btn-danger" @click="updateInfo">提交</a>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div class="tab-pane" id="password">
+                <form class="form-horizontal">
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label">当前密码</label>
+                    <div class="col-sm-10">
+                      <input
+                        type="password"
+                        class="form-control"
+                        v-model="oldpassword"
+                        placeholder="输入当前密码"
+                        :disabled="disabled"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label">新密码</label>
+                    <div class="col-sm-10">
+                      <input
+                        type="password"
+                        class="form-control"
+                        v-model="newpassword"
+                        placeholder="输入新密码"
+                        :disabled="disabled"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label"></label>
+                    <div class="col-sm-10">
+                      <input
+                        type="password"
+                        class="form-control"
+                        v-model="newpassword2"
+                        placeholder="再输入一遍新密码"
+                        :disabled="disabled"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <a class="btn btn-danger" @click="modifyPasswd">提交</a>
                     </div>
                   </div>
                 </form>
@@ -125,18 +143,84 @@
 
 <script>
 import ContentHeader from "@/components/base/ContentHeader.vue";
+import CryptoUtil from "../../util/crypto";
 import MenuApi from "../../common/menu";
+import UserApi from "../../common/user";
 export default {
   data() {
     let userInfo = this.$store.getters.userInfo;
-    let headerInfo = MenuApi.getMenuById(this.Global.Profile);
+    let headerInfo = MenuApi.getMenuById(this.Global.Menu.Profile);
+    let nickname = userInfo.nickname;
+    let description = userInfo.description;
+    let disabled =
+      userInfo.role == this.Global.UserAttr.Role.Guest ? true : false;
+    let oldpassword = "";
+    let newpassword = "";
+    let newpassword2 = "";
     return {
       userInfo,
-      headerInfo
+      headerInfo,
+      nickname,
+      description,
+      oldpassword,
+      newpassword,
+      newpassword2,
+      disabled
     };
   },
   components: {
     ContentHeader
+  },
+  methods: {
+    async updateInfo() {
+      if (this.disabled) {
+        this.$toastr.e("游客不可以编辑哦");
+        return;
+      }
+      let params = {
+        token: this.$store.getters.token,
+        nickname: this.nickname,
+        description: this.description
+      };
+      let res = await UserApi.updateById(params);
+      let { code } = res;
+      if (code == 0) {
+        let userInfo = this.$store.getters.userInfo;
+        userInfo.nickname = this.nickname;
+        userInfo.description = this.description;
+        this.$store.dispatch("setUserInfo", userInfo);
+        this.$toastr.s("更新成功");
+      } else {
+        this.$toastr.e("更新失败，请稍后再试");
+      }
+    },
+    async modifyPasswd() {
+      if (this.disabled) {
+        this.$toastr.e("游客不可以编辑哦");
+        return;
+      }
+      let params = {
+        token: this.$store.getters.token,
+        oldpassword: CryptoUtil.aesEncrypt(this.oldpassword),
+        newpassword: CryptoUtil.aesEncrypt(this.newpassword),
+        newpassword2: CryptoUtil.aesEncrypt(this.newpassword2)
+      };
+      let res = await UserApi.modifyPasswd(params);
+      let { code } = res;
+      if (code == 0) {
+        this.$toastr.s("修改密码成功");
+        if (localStorage.getItem("loginInfo")) {
+          let info = JSON.parse(localStorage.getItem("loginInfo"));
+          info.password = "";
+          localStorage.setItem("loginInfo", JSON.stringify(info));
+        }
+      } else {
+        this.oldpassword = "";
+        this.newpassword = "";
+        this.newpassword2 = "";
+        this.$toastr.e("更新失败，请稍后再试");
+      }
+    }
   }
 };
 </script>
